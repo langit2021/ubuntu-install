b20fb42e6113ade5c96a63ee5aa4a300f683a8fa
langit2021 <lan@dannow.net>
2026-08-18 14:51:06 +0800

    change

--- install.sh
+++ install.sh
@@ -3,11 +3,15 @@
 echo "================================"
 echo " Ubuntu Install Framework"
 echo " Version 0.1"
+echo " curl -sL https://raw.githubusercontent.com/langit2021/ubuntu-install/main/install.sh | bash   "
 echo "================================"
 
 echo "Hello World  pc 2.0"
 
 
+
+sudo systemctl restart systemd-timesyncd
+sudo timedatectl set-timezone Asia/Taipei
 # 把  http:// 改 https://  怕有些防火牆會擋
 sudo sed -i 's|http://|https://|g' /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list 2>/dev/null
 sudo apt update -y
