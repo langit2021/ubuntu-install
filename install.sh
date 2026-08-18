487f776d66ea7271b89aa6b7648d54a0170bbe93
langit2021 <lan@dannow.net>
2026-08-18 15:50:33 +0800

    Update 2026-08-18 15:50:33

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ echo "================================"
 echo " Ubuntu Install Framework"
 echo " Version 0.12"
 echo " curl -sL https://raw.githubusercontent.com/langit2021/ubuntu-install/main/install.sh | sudo bash"
-
 echo "================================"
 echo " Fix date time"
 echo "=== [1/5] 設定系統時區為 Asia/Taipei ==="
@@ -30,6 +29,5 @@ echo "================================"
 sed -i 's|http://|https://|g' /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list 2>/dev/null
 apt update -y
 apt install -y iputils-ping net-tools 
-
 
 echo "=== Installer 完成 ==="
