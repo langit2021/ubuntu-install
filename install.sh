b99e32bb8e939acda2edb637e0c15ef6d77e54c6
langit2021 <lan@dannow.net>
2026-08-18 14:36:11 +0800

    Add APT HTTPS configuration

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ echo " Version 0.1"
 echo "================================"
 
 echo "Hello World  pc 2.0"
+
+
+# 把  http:// 改 https://  怕有些防火牆會擋
+sudo sed -i 's|http://|https://|g' /etc/apt/sources.list.d/ubuntu.sources /etc/apt/sources.list 2>/dev/null
+sudo apt update -y
+sudo apt install -y iputils-ping net-tools 
